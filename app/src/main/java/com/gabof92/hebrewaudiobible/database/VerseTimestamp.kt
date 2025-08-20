b22bc5989e66b9435826fb71a797a015d6b486ed
gabof92 <gabrielflores389@gmail.com>
2025-08-19 21:11:48 -0400

    Created Room database classes: - Database - Entities - DAOs

---
+++ app/src/main/java/com/gabof92/hebrewaudiobible/database/VerseTimestamp.kt
@@ -0,0 +1,18 @@
+package com.gabof92.hebrewaudiobible.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "verse_timestamp")
+data class VerseTimestamp(
+    @PrimaryKey
+    val id: Int,
+
+    @ColumnInfo(name = "verse")
+    val verse :Int,
+    @ColumnInfo(name = "audio_source")
+    val source :Int,
+    @ColumnInfo(name = "milliseconds")
+    val millis :Int,
+)
